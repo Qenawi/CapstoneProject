58c480809b038f1cbcd269b9fcf98fdd24e01465
Qenawi <aldmanmero@yahoo.com>
2017-07-19 18:06:56 +0200

    first

--- app/src/main/java/com/example/qenawi/ttasker_capstone/MainActivity.java
+++ app/src/main/java/com/example/qenawi/ttasker_capstone/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.qenawi.ttasker_capstone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.qenawi.ttasker_capstone.Contract.ContractAcc;
+import com.example.qenawi.ttasker_capstone.Sign.SignInActivity;
+import com.example.qenawi.ttasker_capstone.modle.users_data_modle;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -11,5 +19,38 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        is_user_signed();
     }
-}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1)
+        {
+            if (resultCode == Activity.RESULT_OK)
+            {
+                // sign in finished
+                Toast.makeText(this,"Sign in successfully",Toast.LENGTH_SHORT).show();
+            }
+            if (resultCode == Activity.RESULT_CANCELED)
+            {
+                //Write your code if there's no result
+            }
+        }
+    }
+    private void is_user_signed()
+    {
+        ContractAcc con = new ContractAcc();
+        users_data_modle mUdm = con.get_username();
+        if (mUdm == null)
+        {
+            // Not signed in, launch the Sign In activity
+            startActivityForResult(new Intent(this, SignInActivity.class), 1);
+        }
+        else
+        {
+            Toast.makeText(this,"user is oReady signed in",Toast.LENGTH_SHORT).show();
+            // Call main Activity Create,Join,myprojects
+        }
+        }
+
+    }
---
+++ app/src/main/java/com/example/qenawi/ttasker_capstone/cprovider/ProviderClass.java
@@ -0,0 +1,8 @@
+package com.example.qenawi.ttasker_capstone.cprovider;
+
+/**
+ * Created by QEnawi on 7/19/2017.
+ */
+
+public class ProviderClass {
+}
